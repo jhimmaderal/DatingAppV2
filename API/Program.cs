369ec059767580a2026d2403df5ad0e372ad76e6
jhimmaderal <117509678+jhimmaderal@users.noreply.github.com>
2025-07-12 02:01:09 +0300

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 // // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
 
+builder.Services.AddCors(); // Add Cors for HTTP Request
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.æ
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200")); //Browser Cors Setup
+
 app.MapControllers();
 
 app.Run();
