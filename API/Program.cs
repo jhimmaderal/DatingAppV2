dd752bc37779de8e8b533135d088a7eb3fd0fa49
jhimmaderal <117509678+jhimmaderal@users.noreply.github.com>
2025-07-26 20:49:34 +0300

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -32,8 +33,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 });
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.æ
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200")); //Browser Cors Setup
 
 app.UseAuthentication();
